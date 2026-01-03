f719d95d31832b5605d26d779c9be95d26c48237
bluewine24 <kojh0124@gmail.com>
2026-01-03 13:51:25 +0900

    Read README with utf-8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.5",
     packages=find_packages(),
     description="Context-Aware Automated Feature Engineering (CAAFE) is an automated machine learning tool that uses large language models for feature engineering in tabular datasets. It generates Python code for new features along with explanations for their utility, enhancing interpretability.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Noah Hollmann, Samuel Müller, Frank Hutter",
     author_email="noah.homa@gmail.com",
